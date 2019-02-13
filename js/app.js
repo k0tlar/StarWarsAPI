bb845f282b5f2762eed0cf8d535cf5ed7fa8cd59
Michal Kotlarski <djnumike@gmail.com>
2019-02-13 19:54:56 +0100

    Add errot fuction

--- js/app.js
+++ js/app.js
@@ -11,16 +11,26 @@ $(function(){
 
     function getCharacter(){
         showStats.css('display', 'grid');
-        var numb = Math.floor((Math.random() * 10) + 1);
+        var numb = Math.floor((Math.random() * 50) + 1);
         var apiurl = 'https://swapi.co/api/people/'+ numb + '/';
         axios.get(apiurl).then(function(response){
-            name.text(response.data.name);
-            sex.text(response.data.gender);
-            year.text(response.data.birth_year);
-            height.text(response.data.height);
-            eye.text(response.data.eye_color)
+            updateChar(response)
 
-        });
+        }).catch(function(e){
+            errorChar();
+        })
+    };
+
+    function updateChar(response){
+        name.text(response.data.name);
+        sex.text(response.data.gender);
+        year.text(response.data.birth_year);
+        height.text(response.data.height);
+        eye.text(response.data.eye_color)
+    }
+
+    function errorChar(){
+        name.text('Ther is some problem with that person right now.');
     };
 
     //load();
